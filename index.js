1808200ad12f4c89ff3c1cef3013de6ed03909b9
Asif Ferdous <asifferdous23@gmail.com>
2022-12-28 23:44:59 +0600

    comp

--- index.js
+++ index.js
@@ -23,27 +23,111 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
 
-        const catagoryCollection = client.db('Laptop').collection('Catagory');
+        const taskCollection = client.db('TaskManagement').collection('mytask');
+        const completedCollection = client.db('TaskManagement').collection('completed');
+        const commentCollection = client.db('TaskManagement').collection('comment');
 
-        app.get('/category/:id', async (req, res) => {
-            const code = req.params.id;
-            const query = { code: code };
-            const laptops = await laptopCollection.find(query).toArray();
-            // console.log(laptops);
-            res.send(laptops);
-        });
-
-        app.post('/bookings', async (req, res) => {
-            const bookings = req.body;
-            // console.log(bookings);
-            const result = await bookingsCollection.insertOne(bookings);
+        app.post('/addtask', async (req, res) => {
+            const task = req.body;
+            // console.log(task);
+            const result = await taskCollection.insertOne(task);
             res.send(result);
         });
 
+        app.get('/mytask/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email:email };
+            const task = await taskCollection.find(query).toArray();
+            // console.log(laptops);
+            res.send(task);
+        });
+
+
+        app.delete('/deleteTask/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+
+            const result = await taskCollection.deleteOne(query);
+
+            res.send(result);
+        })
+
+        app.put('/updateTask/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const Item = req.body;
+            console.log(id);
+            const options = { upsert: true };
+            const updatedItem = {
+                $set: {
+                    message:Item.message,
+                    image:Item.image,
+                }
+            }
+            const result =await taskCollection.updateOne(query, updatedItem,options);
+            res.send(result);
+        });
+
+        app.post('/addCompleted/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            
+            const temp = await taskCollection.findOne(query);
+
+            const result =await completedCollection.insertOne(temp);
+            const deleting = await taskCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        app.get('/completed/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email:email };
+            const task = await completedCollection.find(query).toArray();
+            // console.log(laptops);
+            res.send(task);
+        });
+
+        app.delete('/deleteCompleted/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+
+            const result = await completedCollection.deleteOne(query);
+
+            res.send(result);
+        })
+
+       
+        app.post('/addtasktoCompleted/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            
+            const temp = await completedCollection.findOne(query);
+
+            const result =await taskCollection.insertOne(temp);
+            const deleting = await completedCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.post('/comment', async (req, res) => {
+            const comment = req.body;
+             console.log(comment);
+            const result = await commentCollection.insertOne(comment);
+            res.send(result);
+        });
+
+        app.get('/comment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { comment_id:id };
+            const task = await commentCollection.find(query).toArray();
+            // console.log(laptops);
+            res.send(task);
+        });
         
-
-
-
 
     }
     finally {
